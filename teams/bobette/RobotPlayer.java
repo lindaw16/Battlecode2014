c9df1f7217f95a0cf4f127f0ea0999e6b60672c2
Linda Wang <Linda@Lindas-MacBook-Pro-2.local>
2014-01-08 18:01:27 -0500

    meow

---
+++ teams/bobette/RobotPlayer.java
@@ -0,0 +1,124 @@
+package bobette;
+//will not build a pasture if there is not enough cows nearby
+//will not build a pasture if another one is already closeby
+
+import battlecode.common.*;
+import java.util.Random;
+
+public class RobotPlayer
+{
+	public static RobotController rc;
+	static Direction allDirections[] = Direction.values();
+	static Random randall = new Random();
+	
+	public static void run(RobotController rcinput)
+	{
+		rc = rcinput;
+		randall.setSeed(rc.getRobot().getID());
+		while(true)
+		{
+			if (rc.getType() ==RobotType.HQ)
+			{
+				runHeadquarters();				
+			} else if (rc.getType() == RobotType.SOLDIER)
+			  {
+				runSoldier();				
+			  }
+		}
+	}
+			
+			
+	private static void runSoldier(){
+		soldierShoot();
+		soldierMove();
+		rc.yield(); //ends the current round
+	}
+	
+	
+	private static void soldierShoot(){
+		//shooting (prioritized)
+		//array of robots, attack the opponent team
+		Robot[] enemyRobots = rc.senseNearbyGameObjects(Robot.class, 10000, rc.getTeam().opponent());
+		if (enemyRobots.length> 0) //not empty
+		{
+			Robot anEnemy = enemyRobots[0];
+			//need to use RobotInfo b/c Robot doesn't have location
+			RobotInfo anEnemyInfo;
+			try {
+				anEnemyInfo = rc.senseRobotInfo(anEnemy);
+				if (anEnemyInfo.location.distanceSquaredTo(rc.getLocation()) < rc.getType().attackRadiusMaxSquared)
+				{
+					if (rc.isActive())
+					{
+						rc.attackSquare(anEnemyInfo.location);									
+					}
+				}
+			} catch (GameActionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else //no enemies, build a pasture
+		 {
+			if (Math.random() < 0.01 && rc.isActive())
+			{
+				Robot[] nearbyRobots = rc.senseNearbyGameObjects(Robot.class, 10000);
+				boolean isPASTR = false;
+				for (Robot r: nearbyRobots){
+					RobotInfo rInfo;
+					try {
+						rInfo = rc.senseRobotInfo(r);
+						isPASTR = rInfo.type == RobotType.PASTR;
+						break;	
+					} catch (GameActionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}							
+				}
+				if (!isPASTR){
+					System.out.println("bobby is building pasture!");
+					try
+					{
+						rc.construct(RobotType.PASTR);
+					}
+					catch (GameActionException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		 }	
+	}
+	
+	private static void runHeadquarters(){
+		Direction spawnDir = Direction.NORTH;
+		try
+		{
+			//if the robot is active and can move in the spawn direction, and there are less than maximum robots
+			if (rc.isActive() && rc.canMove(spawnDir) && rc.senseRobotCount() < GameConstants.MAX_ROBOTS)
+			{
+				rc.spawn(Direction.NORTH);
+			}
+		} catch (GameActionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void soldierMove(){
+		//movement
+		Direction chosenDirection = allDirections[(int)(randall.nextDouble()*8)];
+		//returns a random direction from the array of directions
+		if (rc.isActive() && rc.canMove(chosenDirection))
+		{
+			try{
+				rc.move(chosenDirection);
+			} catch(GameActionException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+}
+
+
