bb761d53dfe7744d96fccd0f4987577197c78ec3
yuyupuu <yuyupuu@mit.edu>
2014-01-08 19:37:05 -0500

    hello bub

---
+++ teams/bub/RobotPlayer.java
@@ -0,0 +1,17 @@
+package bub;
+
+import battlecode.common.*;
+
+public class RobotPlayer
+{
+	public static void run(RobotController rc)
+	{
+		while(true)
+		{
+			if (rc.getType() == RobotType.HQ){
+				
+			}
+			
+		}
+	}
+}
